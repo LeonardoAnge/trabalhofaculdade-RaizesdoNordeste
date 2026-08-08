edc37a592077c71b704b57669b061a602bbc59b8
Leonardo <leonangelolima@gmail.com>
2026-08-08 17:09:06 -0300

    FastAPI atualizado

--- crud.py
+++ crud.py
@@ -1,19 +1,20 @@
-from database import BackEnd
-from schemas import UsuarioCreate, UsuarioLogin, UsuarioOut, LoginOut, EstoqueBase, EstoqueOut, EstoqueCreate, PedidoCreate, CardapioCreate, CardapioBase, CardapioOut, CardapioUpdate, UnidadeBase, UnidadeOut, UnidadeCreate, PedidoCreate, PedidoUpdate, PedidoOut, ItemPedido
-from fastapi import HTTPException
-import hashlib
-import re
-import sqlite3
+from database import BackEnd #importação do backend no arquivo database
+#importação das classes no aquivo schemas
+from schemas import UsuarioCreate, UsuarioLogin, UsuarioOut, LoginOut, EstoqueBase, EstoqueOut, EstoqueCreate, CardapioCreate, CardapioOut, UnidadeOut, UnidadeCreate, PedidoCreate, PedidoUpdate, PedidoOut 
+from fastapi import HTTPException #extensão do fastapi, para descrever erros.
+import hashlib #Biblioteca para a criação de senhas HASH
+import re #Biblioteca para remodelar os caracteres digitados.
+import sqlite3 #Biblioteca do banco de dados 'SQLite3'
 
-def salvar_cadastro(db: BackEnd, usuario: UsuarioCreate):
-        if usuario.senha != usuario.confirma_senha:
+def salvar_cadastro(db: BackEnd, usuario: UsuarioCreate): # função salvar cadastro
+        if usuario.senha != usuario.confirma_senha:# caso senha for diferente do confirmar senha, vai dar um erro.
             raise HTTPException(status_code= 400, detail= "As senha não coincidem. Digite novamente!")
 
-        cpf_limpo = re.sub(r"\D","", usuario.cpf)
-        if len(cpf_limpo) != 11:
+        cpf_limpo = re.sub(r"\D","", usuario.cpf)#re para tirar qlqer carectere digitado que não seja numeros
+        if len(cpf_limpo) != 11: #len para contabilizar os carecteres digitandos e caso for diferente de 11, returna o Raise
             raise HTTPException(status_code= 400, detail= "CPF inválido")
 
-        senha_hash = hashlib.sha256(usuario.senha.encode("utf-8")).hexdigest()
+        senha_hash = hashlib.sha256(usuario.senha.encode("utf-8")).hexdigest() #Criação de senha HASH
 
         db.conecta_db()
         try:
@@ -21,13 +22,13 @@ def salvar_cadastro(db: BackEnd, usuario: UsuarioCreate):
             INSERT INTO Usuarios (Telefone, Nome, Email, CPF, Data_de_Nascimento, Senha_hash, Cargo)
             VALUES (?, ?, ?, ?, ?, ?, ?)
 
-        """, (usuario.telefone, usuario.nome, usuario.email, cpf_limpo, usuario.data_nascimento , senha_hash, usuario.cargo))
+        """, (usuario.telefone, usuario.nome, usuario.email, cpf_limpo, usuario.data_nascimento , senha_hash, usuario.cargo)) #inserção de dados no banco usuarios
             
             db.conn.commit()
 
-            return UsuarioOut(telefone = usuario.telefone, nome = usuario.nome, email = usuario.email, cpf = cpf_limpo, data_nascimento= usuario.data_nascimento, cargo = usuario.cargo)
+            return UsuarioOut(telefone = usuario.telefone, nome = usuario.nome, email = usuario.email, cpf = cpf_limpo, data_nascimento= usuario.data_nascimento, cargo = usuario.cargo) # return das informações q acabou de serem salvas, onde a senha não sera salva, apenas senha_hash
         
-        except sqlite3.IntegrityError:
+        except sqlite3.IntegrityError: #Caso nao consiga inserir os dados por ja ter cadastrado cpf ou senha, vai dar erro
             raise HTTPException(status_code= 409, detail= "Este CPF ou Email ja está cadastrado.")
         finally:
             db.desconecta_db()
@@ -38,17 +39,17 @@ def verificar_login(db: BackEnd, usuario: UsuarioLogin):
             db.cursor.execute("""
             SELECT Cargo, Senha_hash, Nome FROM Usuarios WHERE email =?
             """, (usuario.email,))
-            usuario_encontrado = db.cursor.fetchone()
+            usuario_encontrado = db.cursor.fetchone() #fetchone para salvar a linha de dados que foi puxada.
             if not usuario_encontrado:
                 raise HTTPException(status_code= 401, detail="E-mail ou senha inválidos.")
-
+            #como foi puxado 3 dados, usamos o [] para identificar cada tipo de dado e damos um nome.
             cargo_banco = usuario_encontrado[0]
             hash_banco = usuario_encontrado[1]
             nome_user = usuario_encontrado[2]
 
-            hash_digitado = hashlib.sha256(usuario.senha.encode("utf-8")).hexdigest()
+            hash_digitado = hashlib.sha256(usuario.senha.encode("utf-8")).hexdigest() #senha digitada pelo usuario
 
-            if hash_digitado == hash_banco:
+            if hash_digitado == hash_banco: #caso senha digitada pelo usuario for diferente de senha do banco, vai da erro
                 return LoginOut(cargo = cargo_banco, nome = nome_user,mensagem= "Login realizado com sucesso.")
                 
             raise HTTPException(status_code= 401, detail="E-mail ou senha inválidos.")
@@ -63,10 +64,10 @@ def cadastrar_prato (db: BackEnd, produto: CardapioCreate):
         INSERT INTO Cardapio (Prato, Preco)
         VALUES (?, ?)
 """, (produto.prato, produto.preco))
-        db.conn.commit()
-        id = db.cursor.lastrowid
+        db.conn.commit() #Commit para salvar no banco de dados
+        id = db.cursor.lastrowid #lastrowid, para pegar a ultima linha de id registrada
 
-        return CardapioOut ( id = id, prato = produto.prato, preco = produto.preco )
+        return CardapioOut ( id = id, prato = produto.prato, preco = produto.preco ) # return do schema CardapioOut
                 
     except sqlite3.IntegrityError:
         raise HTTPException(status_code= 409, detail="Prato ja cadastrado!")
@@ -104,9 +105,9 @@ def criar_unidade(db: BackEnd, unidade: UnidadeCreate):
         SELECT Cargo FROM Usuarios
         WHERE Telefone = ?
     """, (unidade.gerente,))
-        gerente = db.cursor.fetchone()
-        cargo = gerente[0]
-        if cargo != "Gerente":
+        gerente = db.cursor.fetchone() #fetchone para salvar os dados
+        cargo = gerente[0] #pego a variavel cargo e deu o valor gerente [0]
+        if cargo != "Gerente":#apenas usuarios com cargo gerente podem ser registrados como responsavel por unidade.
             raise HTTPException(status_code= 404, detail= "Usuario selecionado nao possui o cargo 'Gerente'!")
 
         db.cursor.execute("""
@@ -128,14 +129,14 @@ def listar_unidade(db:BackEnd):
         db.cursor.execute("""
         SELECT id, Nome, Gerente FROM Unidade
     """)
-        unidades = db.cursor.fetchall()
-        if not unidades:
+        unidades = db.cursor.fetchall()#fetchal para pegar todas linhas das colunas selecionadas da tabela Unidade.
+        if not unidades: #caso nao tenha nenhuma unidade registrada, da erro raise.
             raise HTTPException(status_code= 404, detail= "Nenhuma unidade encontrada em nosso sistema")
     
         return [{"id": unidade [0],
                 "nome" : unidade [1],
                 "gerente" : unidade [2]}
-                for unidade in unidades]
+                for unidade in unidades] # return dos dados da tabela para o main.
         
     except sqlite3.IntegrityError as erro:
         raise HTTPException(status_code= 500, detail= f"Erro no banco de dados: {erro}")
@@ -143,7 +144,7 @@ def listar_unidade(db:BackEnd):
     finally:
         db.desconecta_db()
 
-def listar_pratos(db: BackEnd, limit : int = 50, offset: int = 0):
+def listar_pratos(db: BackEnd, limit : int = 50, offset: int = 0): #limit para o maximo de pratos a returna e offset para pular os id.
     db.conecta_db()
     try:
         db.cursor.execute("""
@@ -216,27 +217,27 @@ def listar_pedidos(db: BackEnd):
     finally:
         db.desconecta_db()
 
-def adicionar_estoque(db: BackEnd, estoque: EstoqueBase):
+def adicionar_estoque(db: BackEnd, estoque: EstoqueBase): #função para saber se o produto foi registrado ou não no estoque
     produto = buscar_estoque(db, estoque)
-    if produto:
+    if produto:# caso achou um produto registrado, vai para a função atualizar quantidade
         return atualizar_quantidade (db, estoque)
 
-    else:
+    else: # caso não tenha achado o produto, vai para função cadastrar_produto
         return cadastrar_produto (db, estoque)
 
-def buscar_estoque(db: BackEnd, estoque: EstoqueBase):
+def buscar_estoque(db: BackEnd, estoque: EstoqueBase):#função para buscar na tabela Estoque, para saber se ja tem o pedido
     db.conecta_db()
     try:
         db.cursor.execute("""
         SELECT * FROM Estoque
         WHERE Unidade_id = ? AND Cardapio_id = ?
         """, (estoque.unidade_id, estoque.cardapio_id)) 
-        return db.cursor.fetchone()
+        return db.cursor.fetchone() #return o valor para a função adicionar_estoque
           
     finally:
         db.desconecta_db()
 
-def atualizar_quantidade (db:BackEnd, estoque:EstoqueBase):
+def atualizar_quantidade (db:BackEnd, estoque:EstoqueBase):#função para adicionar a quantidade dos produtos no estoque
     db.conecta_db()
     try:
         db.cursor.execute("""
@@ -251,7 +252,7 @@ def atualizar_quantidade (db:BackEnd, estoque:EstoqueBase):
             FROM Estoque
             INNER JOIN Cardapio ON Estoque.Cardapio_id = Cardapio.id
             WHERE Estoque.Unidade_id = ? AND Estoque.Cardapio_id = ?
-            """, (estoque.unidade_id, estoque.cardapio_id))
+            """, (estoque.unidade_id, estoque.cardapio_id)) #pega os valores das tabelas Cardapio e Estoque, para salva no fetchone.
     
         estoque_atualizado = db.cursor.fetchone()
         return EstoqueOut (id = estoque_atualizado [0], prato = estoque_atualizado [1], preco = estoque_atualizado[2], quantidade= estoque_atualizado [3])
@@ -261,16 +262,16 @@ def atualizar_quantidade (db:BackEnd, estoque:EstoqueBase):
 
 def criar_pedido (db: BackEnd, pedido: PedidoCreate):
         db.conecta_db()
-        valor_total_pedido = 0.0
+        valor_total_pedido = 0.0 #valor inicial do pedido
         try:
-            for item in pedido.itens:
+            for item in pedido.itens: #um for para passar por todas lista por itens
                 db.cursor.execute(""" 
                 SELECT Quantidade FROM Estoque WHERE Unidade_id = ? AND Cardapio_id = ? 
                 """,(pedido.unidade_id, item.cardapio_id))
                 produto = db.cursor.fetchone()
-                quantidade = produto [0]
+                quantidade = produto [0] #pega a quantidade de produtos no Estoque
 
-                if quantidade < item.quantidade:
+                if quantidade < item.quantidade: #caso a quantidade do Estoque for menor q a quantidade pedida, vai dar erro raise
                     raise HTTPException(status_code= 409, detail="Quantidade insuficiente em estoque.")
                      
 
@@ -279,28 +280,28 @@ def criar_pedido (db: BackEnd, pedido: PedidoCreate):
                 """,(item.cardapio_id,))
                 valor = db.cursor.fetchone()
                 preco_unitario = valor [0]
-                total_pagar = preco_unitario * item.quantidade
+                total_pagar = preco_unitario * item.quantidade #total a pagar é o valor unitario vezes quantidade.
 
-                valor_total_pedido += total_pagar
+                valor_total_pedido += total_pagar #valor total vai adicionando a cada pedido feito.
 
                 db.cursor.execute("""
                 UPDATE Estoque
                 SET Quantidade = Quantidade - ?
                 WHERE Unidade_id = ? AND Cardapio_id = ?
-                """,(item.quantidade, pedido.unidade_id, item.cardapio_id))
+                """,(item.quantidade, pedido.unidade_id, item.cardapio_id))#Retirar produtos do estoque, apos pedido ser feito
 
             db.cursor.execute("""
             INSERT INTO Pedidos (Canal, Cliente_telefone, Forma_pagamento, Unidade_id, Total, Status_pagamento, Status_pedido)
             VALUES (?, ?, ?, ?, ?, ?, ?)
-            """,(pedido.canalPedido, pedido.cliente_telefone, "MOCK", pedido.unidade_id, valor_total_pedido, "AGUARDANDO_PAGAMENTO", "AGUARDANDO_PAGAMENTO"))
+            """,(pedido.canalPedido, pedido.cliente_telefone, "MOCK", pedido.unidade_id, valor_total_pedido, "AGUARDANDO_PAGAMENTO", "AGUARDANDO_PAGAMENTO")) #Inserir valores na tabela pedido
 
-            pedido_id = db.cursor.lastrowid
+            pedido_id = db.cursor.lastrowid #pega o ultimo id registrado.
 
             for item in pedido.itens:
                 db.cursor.execute("""
                 INSERT INTO ItensPedido (Pedido_id, Cardapio_id, Quantidade)
                 VALUES (?, ? ,?)
-                    """, (pedido_id, item.cardapio_id, item.quantidade)) 
+                    """, (pedido_id, item.cardapio_id, item.quantidade))  #Para inserir itens pedidos na tabela
                     
             db.conn.commit()
             
@@ -313,7 +314,7 @@ def criar_pedido (db: BackEnd, pedido: PedidoCreate):
         finally:
              db.desconecta_db()
 
-def status_pedido(db: BackEnd, pedido: PedidoUpdate, id_pedido):
+def status_pedido(db: BackEnd, pedido: PedidoUpdate, id_pedido):# função para alterar o status_pagamento
     db.conecta_db()
     try:
         db.cursor.execute("""
@@ -334,7 +335,7 @@ def status_pedido(db: BackEnd, pedido: PedidoUpdate, id_pedido):
             db.cursor.execute("""
             SELECT Cardapio_id, Quantidade FROM ItensPedido
             WHERE Pedido_id = ?     
-""",(id_pedido,))
+""",(id_pedido,)) # caso o pagamento for recusado, vai returna a quantidade de produtos para o estoque
             
             retornar_quantidade = db.cursor.fetchall()
             for itens in retornar_quantidade:
@@ -362,7 +363,7 @@ def status_pedido(db: BackEnd, pedido: PedidoUpdate, id_pedido):
     finally:
         db.desconecta_db()
 
-def atualizar_pedido_cozinha(db:BackEnd, id_pedido):
+def atualizar_pedido_cozinha(db:BackEnd, id_pedido):#função para os cozinheiros atualizar caso o pedido esteja pronto
     db.conecta_db()
     try:
         db.cursor.execute("""
@@ -386,19 +387,19 @@ def atualizar_pedido_cozinha(db:BackEnd, id_pedido):
                 "Forma_pagamento ": pedido_pronto[4],
                 "Total ": pedido_pronto[5],
                 "Status_pagamento" : pedido_pronto[6],
-                "Status_pedido ": pedido_pronto[7],}
+                "Status_pedido ": pedido_pronto[7]} #return os dados para o main
     
     finally:
         db.desconecta_db()
 
-def atualizar_prato(db:BackEnd, usuario: UsuarioLogin, preco, prato_id):
+def atualizar_prato(db:BackEnd, usuario: UsuarioLogin, preco, prato_id):#função para atualizar o preço dos pratos
     db.conecta_db()
     try:
         db.cursor.execute("""
         SELECT Cargo, Senha_hash FROM Usuarios WHERE email =?
         """, (usuario.email,))
         usuario_encontrado = db.cursor.fetchone()
-        if not usuario_encontrado:
+        if not usuario_encontrado: #Caso nao ache o usuario, por ter digitado senha ou email invalido, return raise
             raise HTTPException(status_code= 401, detail="E-mail ou senha inválidos.")
         
         cargo_banco = usuario_encontrado[0]
@@ -409,7 +410,7 @@ def atualizar_prato(db:BackEnd, usuario: UsuarioLogin, preco, prato_id):
         if hash_digitado != hash_banco:
             raise HTTPException(status_code= 401, detail="E-mail ou senha inválidos.")
         
-        if cargo_banco != "Gerente":
+        if cargo_banco != "Gerente":#return raise, para caso o cargo do usuario for diferente de gerente
             raise HTTPException(status_code= 403, detail="Usuario não tem permissão para atualizar prato!")                  
 
         db.cursor.execute("""
@@ -418,7 +419,7 @@ def atualizar_prato(db:BackEnd, usuario: UsuarioLogin, preco, prato_id):
         WHERE id = ?
         """, (preco, prato_id))
                             
-        if db.cursor.rowcount == 0:
+        if db.cursor.rowcount == 0: #return raise, para caso a contagem de linhas atualizadas seja igual a 0
             raise HTTPException(status_code= 404, detail="Prato não encontrado em nosso sistema!")
                             
         db.conn.commit()
@@ -437,7 +438,7 @@ def excluir_prato(db: BackEnd, prato_id: int):
     try:
         db.cursor.execute("""
         DELETE FROM Cardapio WHERE id = ?
-    """, (prato_id,))
+    """, (prato_id,)) #deleta os pratos com o id enviado
 
         if db.cursor.rowcount == 0:
             raise HTTPException(status_code= 404, detail="Prato não encontrado em nosso sistema!")
--- database.py
+++ database.py
@@ -2,18 +2,18 @@ import sqlite3
 from models import CRIAR_TABELA_USUARIOS, CRIAR_TABELA_UNIDADE, CRIAR_TABELA_CARDAPIO, CRIAR_TABELA_ESTOQUE,CRIAR_TABELA_PEDIDOS, CRIAR_TABELA_ITENS_PEDIDO
 
 class BackEnd:
-    def conecta_db(self):
+    def conecta_db(self): #Conectar ao banco de dados
         self.conn = sqlite3.connect("Sistema_cadastros.db")
         self.cursor = self.conn.cursor()
 
-    def desconecta_db(self):
+    def desconecta_db(self): #Desconectar do banco
         self.conn.close()
 
-    def cria_tabela(self):
+    def cria_tabela(self): #criação das tabelas no banco
         self.conecta_db()
 
-        #self.cursor.execute("""DROP TABLE Pedidos""")
-        self.cursor.execute("PRAGMA foreign_keys = ON;")
+        self.cursor.execute("PRAGMA foreign_keys = ON;") # para habilitar o foreign key
+        #criação de tabelas, q foram puxazdas do models
         self.cursor.execute(CRIAR_TABELA_USUARIOS)
         self.cursor.execute(CRIAR_TABELA_UNIDADE)
         self.cursor.execute(CRIAR_TABELA_CARDAPIO)
@@ -21,7 +21,7 @@ class BackEnd:
         self.cursor.execute(CRIAR_TABELA_PEDIDOS)
         self.cursor.execute(CRIAR_TABELA_ITENS_PEDIDO)
 
-        self.conn.commit()
+        self.conn.commit() #Salvar no Banco de dados
         self.desconecta_db() 
             
             
